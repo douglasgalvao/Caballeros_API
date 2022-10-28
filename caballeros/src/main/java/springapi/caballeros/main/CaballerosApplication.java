62a1e5a1fd97eb96dec3581cd0d247258dd4f001
douglasgalvao <douglasgmachado98@gmail.com>
2022-10-28 14:37:53 -0300

    Api Caballeros 28/10/22 - Database is connected

---
+++ caballeros/src/main/java/springapi/caballeros/main/CaballerosApplication.java
@@ -0,0 +1,13 @@
+package springapi.caballeros.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaballerosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaballerosApplication.class, args);
+	}
+
+}
---
+++ caballeros/src/test/java/springAPI/caballeros/CaballerosApplicationTests.java
@@ -0,0 +1,13 @@
+package springAPI.caballeros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaballerosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
