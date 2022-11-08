f998db12925543ff93ab1babdeda5db54fcc67de
douglasgalvao <douglasgmachado98@gmail.com>
2022-11-08 03:22:36 -0300

    Refactor Trying to deploy

---
+++ caballeros/src/test/java/springapi/caballeros/CaballerosApplicationTest.java
@@ -0,0 +1,16 @@
+package springapi.caballeros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration
+class CaballerosApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+ 
